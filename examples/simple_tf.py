42a5decd3c749a5fffa1510477902030e7909e9b
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-09-01 18:16:33 -0700

    simple TensorFlow example

---
+++ examples/simple_tf.py
@@ -0,0 +1,16 @@
+#!/bin/env python
+import sys
+
+if not sys.argv[1:]:
+  import ncluster
+  task = ncluster.make_task(instance_type='t3.micro')
+  task.upload(__file__)
+  task.run('pip install tensorflow')
+  task.run(f'python {__file__} worker')
+elif sys.argv[1] == 'worker':
+  import tensorflow as tf
+  sess = tf.Session()
+  ones = tf.ones((1000,1000))
+  result = sess.run(tf.matmul(ones, ones))
+  print(f"matmul gave {result.sum()}")
+
