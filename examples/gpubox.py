f05d9ada908ee60d311109b0f64c68bdb71e97b4
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-09-06 17:42:25 -0700

    clean-up

--- examples/gpubox.py
+++ examples/gpubox.py
@@ -6,12 +6,11 @@ import argparse
 import os
 import ncluster
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', type=str, default='gpubox',
                     help="instance name")
 parser.add_argument('--image-name', type=str,
-                    default='', # 'Deep Learning AMI (Ubuntu) Version 12.0',
+                    default='',  # 'Deep Learning AMI (Ubuntu) Version 12.0',
                     help="name of AMI to use ")
 parser.add_argument('--instance-type', type=str, default='g3.4xlarge',
                     help="type of instance")
@@ -26,7 +25,7 @@ def main():
   task = ncluster.make_task(name=args.name,
                             instance_type=args.instance_type,
                             image_name=args.image_name)
-  
+
   # upload notebook config with provided password
   jupyter_config_fn = _create_jupyter_config(args.password)
   remote_config_fn = '/home/ubuntu/.jupyter/jupyter_notebook_config.py'
@@ -46,11 +45,12 @@ def _create_jupyter_config(password):
   from notebook.auth import passwd
   sha = passwd(args.password)
   local_config_fn = f'{module_path}/gpubox_jupyter_notebook_config.py'
-  temp_config_fn = '/tmp/'+os.path.basename(local_config_fn)
+  temp_config_fn = '/tmp/' + os.path.basename(local_config_fn)
   os.system(f'cp {local_config_fn} {temp_config_fn}')
   _replace_lines(temp_config_fn, 'c.NotebookApp.password',
                  f"c.NotebookApp.password = '{sha}'")
   return temp_config_fn
+
 
 def _replace_lines(fn, startswith, new_line):
   """Replace lines starting with starts_with in fn with new_line."""
@@ -62,7 +62,7 @@ def _replace_lines(fn, startswith, new_line):
       new_lines.append(line)
   with open(fn, 'w') as f:
     f.write('\n'.join(new_lines))
-  
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
   main()
