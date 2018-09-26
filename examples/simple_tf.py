9b8ce6e36ca038b4f8ce3f98cec98a60c41ea638
Yaroslav Bulatov <yaroslavvb@gmail.com>
2018-09-26 14:54:46 -0700

    Update simple_tf.py

--- examples/simple_tf.py
+++ examples/simple_tf.py
@@ -9,8 +9,10 @@ if not sys.argv[1:]:
   task.run(f'python {__file__} worker')
 elif sys.argv[1] == 'worker':
   import tensorflow as tf
+  import os
   sess = tf.Session()
   ones = tf.ones((1000,1000))
   result = sess.run(tf.matmul(ones, ones))
   print(f"matmul gave {result.sum()}")
+  os.system('sudo shutdown -h -P 10')  # shut down instead in 10 mins
 
